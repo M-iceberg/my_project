23cd255cfd7f1d8e8158faf0ca254c81bcbfc63b
Bingyi Yang <33240528+M-icerberg@users.noreply.github.com>
2017-12-06 14:27:02 -0500

    Update monster.py

--- monster.py
+++ monster.py
@@ -4,12 +4,11 @@ import pygame
 class Monster(pygame.sprite.Sprite):
 
     def __init__(self,name,x,y,img_file):
-
+        '''general function description: initialze the sprite methods, the position of the monster object, get the image file
+        param list:(str)name,(int)x,(int)y,(str)img_file
+        return:none'''
+        
         pygame.sprite.Sprite.__init__(self)
-
-
-
-
 
         self.image = pygame.image.load(img_file).comvert_alpha()
 
@@ -25,7 +24,10 @@ class Monster(pygame.sprite.Sprite):
 
 
     def move(self):
-
+        '''general function description: create a method to move the monster object
+        param list:none
+        return:none'''
+        
         self.rect.x +=self.speed
 
     
