88ebd30350b20e2bcd3ff0bb2b5022819bed41ad
Bingyi Yang <33240528+M-icerberg@users.noreply.github.com>
2017-12-06 15:15:36 -0500

    Update Mycharacter.py

--- Mycharacter.py
+++ Mycharacter.py
@@ -7,9 +7,11 @@ import random
 class Mycharacter(pygame.sprite.Sprite):
 
     def __init__(self,coordinates,img_file):
-
+        '''general function description:create the rect object for the character, initialize the size and speed of the character
+        param list:(tuple)coordinates,(str)img_file
+        return:none'''
+        
         #initialize all the Sprite functionality
-
         pygame.sprite.Sprite.__init__(self)
 
 
@@ -22,11 +24,10 @@ class Mycharacter(pygame.sprite.Sprite):
 
 
         #create surface object image
-
         self.image=pygame.image.load(img_file).convert_alpha()
 
 
-        #get the rectangle for posotioning
+        #get the rectangle for positioning
 
         self.rect=self.image.get_rect()
         self.image = pygame.transform.scale(self.image, (35,35))
@@ -44,23 +45,38 @@ class Mycharacter(pygame.sprite.Sprite):
     #methods to move
 
     def move_up(self):
+        '''general function description: update the y coordinate to move upward the object
+        param list:none
+        return:none'''
 
         self.rect.y -= self.speed
 
     def move_down(self):
-
+        '''general function description: update the y coordinate to move downward the object
+        param list:none
+        return:none'''
+        
         self.rect.y += self.speed
 
     def move_left(self):
-
+        '''general function description: update the x coordinate to move leftward the object
+        param list:none
+        return:none'''
+        
         self.rect -=self.speed
 
     def move_right(self):
-
+        '''general function desciption: update the x coordinate to move rightward the object
+        param list:none
+        return:none'''
+        
         self.rect +=self.speed
 
 
 
     def update(self):
-
+        '''general function description: tell the user the screen is updating
+        param list:none
+        return:none'''
+        
         print("updating position")
